086a8fd4d1a2e794dd02de855e7d055cb9c46f3c
Buket Ulu <bkt.ulu06@gmail.com>
2021-10-10 18:31:24 +0300

    Davetli ekle butonu ile modal eklenmiştir.

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,5 +1,72 @@
 <template>
-    <p>Test Ediyorum. {{id}} {{buket}}</p>
+    <v-app>
+        <div class="container" style="text-align: center">
+        <v-row justify="center">
+        <div class="col-md-12">
+            <v-dialog
+            v-model="dialog"
+            persistent
+            max-width="600px"
+            >
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                color="primary"
+                dark
+                v-bind="attrs"
+                v-on="on"
+                >
+                Davetli Ekle
+                </v-btn>
+            </template>
+            <v-card>
+                <v-card-title>
+                <span class="text-h5">Davetli Ekle</span>
+                </v-card-title>
+                <v-card-text>
+                <v-container>
+                    <v-row>
+                    
+                    <v-col cols="12">
+                        <v-text-field
+                        label="First Name"
+                        required
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                        <v-text-field
+                        label="Last Name"
+                        required
+                        ></v-text-field>
+                    </v-col>
+                    
+                    
+                    </v-row>
+                </v-container>
+                </v-card-text>
+                <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="dialog = false"
+                >
+                    Kapat
+                </v-btn>
+                <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="save()"
+                >
+                    Kaydet
+                </v-btn>
+                </v-card-actions>
+            </v-card>
+            
+            </v-dialog>
+            </div>
+        </v-row>
+        </div>
+        </v-app>
 </template>
 <script>
 
@@ -7,7 +74,7 @@ export default ({
     data(){
         return{
             id: this.$route.params.id,
-            buket:"Buket"
+            dialog: false,
         }
     }
 })
